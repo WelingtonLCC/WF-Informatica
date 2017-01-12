b75f9e60a0327cdeba3139d0b9c0150b48985f52
WelingtonLuiz <welington.c.campos@hotmail.com>
2017-01-12 09:59:41 -0200

    Commit unicial

---
+++ componentes/nav.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<html>
+	<head>
+		
+		<link rel="stylesheet" href="../css/nav.css">
+		<link rel="stylesheet" href="../bower_components/font-awesome/css/font-awesome.min.css">
+	</head>
+    <body>
+        <nav>
+        	<div class="nav-left">
+        		<ul>
+        			<li><a href="#">Home</a></li>
+        			<li><a href="#">Produtos</a></li>
+        			<li><a href="#">Serviços</a></li>
+        			<li><a href="#">Contato</a></li>
+        			<li><a href="#">Empresa</a></li>
+        		</ul>	
+        	</div>
+        	<div class="nav-right">
+        		<input class=" inp-pesquisar" type="text" name="pesquisar" placeholder="Pesquisar...">
+        	</div>
+        </nav>
+    </body>
+</html>
---
+++ index.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html lang="pt-br">
+    <head>
+        <meta charset="utf-8">
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Manutenção de micro, notebooks, celulares, tablets, impressoras, sistemas de segurança, cercas eletricas e mauitos mais em Santa Barbara D'Oeste WF Informatica</title>
+    </head>
+    <body>
+        <div class="nav-index">
+        </div>
+        <div class="banner-index"></div>
+        <div class="main-index"></div>
+        <div class="footer-index"></div>
+    </body>
+</html>
