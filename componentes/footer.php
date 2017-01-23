db3ea4c30d4b0758c4493a5318c13fad9644c4bd
WelingtonLuiz <welington.c.campos@hotmail.com>
2017-01-22 22:46:42 -0200

    Fotter falta os textos

--- componentes/footer.php
+++ componentes/footer.php
@@ -8,7 +8,7 @@
         <div class="footer">
         	<div class="endereco">
         		<address>
-        			Rua Independencia 602 Rochelle, Santa Barbara D'Oeste
+        			<u>Rua Independencia 602 Rochelle, Santa Barbara D'Oeste</u>
         		</address>
         	</div>
         	<div class="bandeiras">
@@ -16,17 +16,64 @@
 	        		<div><i class="fa fa-cc-mastercard" aria-hidden="true"></i></div>
 	        		<div><i class="fa fa-cc-visa" aria-hidden="true"></i></div>
 	        		<div><i class="fa fa-cc-diners-club" aria-hidden="true"></i></div>
-	        		<div><img src="" alt=""></div>
-	        		<div><img src="" alt=""></div>
+	        		<div><i class="fa fa-cc-diners-club" aria-hidden="true"></i></div>
+	        		<div><i class="fa fa-cc-diners-club" aria-hidden="true"></i></div>
 	        	</div>
                 <div class="redes">
-                    <div><img src="" alt=""><a href=""></a></img></div>
-                    <div><img src="" alt=""><a href=""></a></img></div>
-                    <div><img src="" alt=""><a href=""></a></img></div>
-                    <div><img src="" alt=""><a href=""></a></img></div>
+                    <div><i class="fa fa-map-marker mapa" aria-hidden="true"></i></div>
+                    <div><i class="fa fa-facebook face" aria-hidden="true"></i></div>
+                    <div><i class="fa fa-twitter twiter" aria-hidden="true"></i></div>
+                    <div><i class="fa fa-google-plus gplus" aria-hidden="true"></i></div>
                 </div>
         	</div>
-        	<div class="footer-servicos"></div>
+        	<div class="footer-servicos">
+                <div class="painel">
+                    <h4>Suporte <i class="fa fa-wrench" aria-hidden="true"></i></h4>
+                    <ul>
+                        <li>Desktops</li>
+                        <li>Notebooks</li>
+                        <li>Tablets</li>
+                        <li>Celulares</li>
+                        <li>Impressoras</li>
+                    </ul>
+                </div>
+                <div class="painel">
+                    <h4>Comercio <i class="fa fa-cart-plus" aria-hidden="true"></i></h4>
+                      <ul>
+                        <li>Lorem ipsum</li>
+                        <li>Lorem ipsum</li>
+                        <li>Lorem ipsum</li>
+                        <li>Lorem ipsum</li>
+                        <li>Lorem ipsum</li>
+                    </ul>
+                </div>
+                <div class="painel">
+                    <h4>Escritório <i class="fa fa-briefcase" aria-hidden="true"></i></h4>
+                      <ul>
+                        <li>Lorem ipsum</li>
+                        <li>Lorem ipsum</li>
+                        <li>Lorem ipsum</li>
+                        <li>Lorem ipsum</li>
+                        <li>Lorem ipsum</li>
+                        <li>Lorem ipsum</li>
+                        <li>Lorem ipsum</li>
+                    </ul>
+                </div>
+                <div class="painel">
+                    <h4>Empresas <i class="fa fa-building" aria-hidden="true"></i></h4>
+                      <ul>
+                        <li>Lorem ipsum</li>
+                        <li>Lorem ipsum</li>
+                        <li>Lorem ipsum</li>
+                        <li>Lorem ipsum</li>
+                        <li>Lorem ipsum</li>
+                    </ul>
+                </div>   
+            </div>
+            <div class="clear"></div>
+        </div>
+        <div class="adrees">
+            <p>Todos os direitos Reservados WF-Informática </p>
         </div>
     </body>
 </html>
